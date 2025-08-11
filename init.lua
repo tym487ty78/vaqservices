faa5b849c9b08dedecce5a05b2def8162f5320e2
vaqservices <xvateq@gmail.com>
2025-08-12 00:40:30 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+-- commenting it here because u prob think you gained anything.
+-- no u didnt, all this is testing and the original source is going to be obfuscated and unreadable.
+-- if u think "yeah but atleast ik how it works", testing are unprotected however main files will be.
+
 local keys = {"123456","1234"}
 local initalized = false
 local functions = {}
