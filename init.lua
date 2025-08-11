b3f7259e4a2cd67b8a7fb44c0718063ec6d995f8
vaqservices <xvateq@gmail.com>
2025-08-12 01:33:10 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,7 +4,6 @@
 
 local keys = {"123456","1234"}
 local initalized = false
-local functions = {}
 
 local init = function(key)
   if table.find(keys,key) then
@@ -17,13 +16,6 @@ end
 
 local function1 = function(arg)
   if initalized then print(arg1) end
-end
-
-table.insert(functions,functcion1)
-table.insert(functions,init)
-
-local getfunctions = function()
-  return functions
 end
 
 return {init=init,function1=function1}
