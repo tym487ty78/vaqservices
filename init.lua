5a091c5789a94e5992785d106543cb853bc68613
vaqservices <xvateq@gmail.com>
2025-08-12 00:32:52 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,15 @@
+local keys = {"123456","1234"}
+local initalized = false
 
+local init = function(key)
+  if table.find(keys,key) then
+    print("initalized premium version!")
+  else
+    print("initalized basic version.")
+  end
+  initalized = true
+end
+
+local function1 = function(arg)
+  if initalized then print(arg1) end
+end
